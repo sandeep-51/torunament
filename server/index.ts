a4f0041c130be32e7cd26fe7480e56fe696ae1d4
sandeep-51 <sandeepnath2063@gmail.com>
2025-11-08 14:59:59 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -67,14 +67,17 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on the port specified in the environment variable PORT
+ 
+  
+    // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
-  // this serves both the API and the client.
+  // This serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-  const port = parseInt(process.env.PORT || '5000', 10);
-server.listen(port, "0.0.0.0", () => {
-  log(`✅ Server running on port ${port}`);
-});
+
+  server.listen(port, "0.0.0.0", () => {
+    log(`✅ Server running on port ${port}`);
+  });
+
 
 })();
